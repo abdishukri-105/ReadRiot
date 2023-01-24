21d8ebe44aea63a79724e3ba60c2ec7407f56e3e
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-24 09:21:40 +0300

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <h1 className='bg-danger fst-italic fw-bolder text-primary' >READRIOT</h1>
+    </div>
+  );
+}
+
+export default App;
