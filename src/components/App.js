3f72ce6835a5fbd374ccb85c064723c976aea402
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-24 17:47:33 +0300

    added react-strap

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import '../App.css';
 import NavBar from './NavBar';
 // import Home from './Home'
 import Library from './Library';
+import Login from './Login';
 
 
 function App() {
   return (
     <div className="">
          <NavBar />
+         <Login />
         {/* <Home /> */} 
          <Library /> 
     </div>
