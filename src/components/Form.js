8991d4c6c7c4023badbfc5ad866f17b7cc74fae2
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-01-24 15:02:06 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 const Form = () => {
     return ( 
-        Form
+        home
      );
 }
  
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
 
 
-const NavBar = () => {
-    return ( 
-      
-        <nav>
-            Navbar
-        </nav>
-        
-        
-     );
+
+
+function Navbar() {
+    
+  return (
+  
+
+  <div>   
+  <nav class="navbar bg-warning d-flex justify-content-between">
+  <div class="m-5">
+    <h2 class="navbar-brand ">READRIOT</h2>
+  </div>
+  <div>
+    <button className="btn btn-outline-primary">logout</button>
+  </div>
+</nav>
+
+
+   </div>
+
+    
+  )
 }
- 
-export default NavBar;
+  export default Navbar
