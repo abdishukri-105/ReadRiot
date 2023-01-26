561a0d5c354295a52a00f21ba8f8549e950e9b76
Lawrence137 <lawrencekimani18@gmail.com>
2023-01-26 12:48:54 +0300

    sign-up form working

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import '../App.css';
 import NavBar from './NavBar';
 import Home from './Home'
 // import Library from './Library';
+import Login from './Login';
+
 
 
 function App() {
@@ -11,7 +13,9 @@ function App() {
     <div className="">
         <NavBar />
          <Home />
+         < Login />
          {/* <Library /> */}
+         
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState  } from 'react';
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function Login() {
   const [showSignUp, setShowSignUp] = useState(false);
-
   const handleClick = () => {
     setShowSignUp(!showSignUp);
   }
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
+  
   const handleLogin = () => {
-    // check if the entered username and password match any saved details
-    if (username === "savedUsername" && password === "savedPassword") {
-      // allow the user to login
-    } else {
-      setShowSignUp(true);
-    }
+    alert("Login successful!");
   }
 
-  const newUser = {username: "newUser", password: "newPass"};
-
-fetch('/db.json', {
-  method: 'POST',
-  body: JSON.stringify(newUser),
-  headers: { 'Content-Type': 'application/json' }
-})
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err));
-
-
-fetch('/db.json')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
-
-  
-  
+  const handleSignup = () => {
+    setShowSignUp(false);
+}
 
 
   return (
@@ -51,20 +23,21 @@ fetch('/db.json')
         <Col xs="12" md={{ size: 6, offset: 3 }} className={showSignUp ? "d-none" : ""}>
           <Form>
             <h2 className="text-center">Sign In</h2>
+
             <FormGroup>
               <Label for="username"></Label>
               <Col xs="12" md={{ size: 6, offset: 3 }}>
-   <Input type="text" placeholder="username" required onChange={e => setUsername(e.target.value)} />
+              <Input type="text" placeholder="username" required />
               </Col>
             </FormGroup>
+
             <FormGroup>
               <Label for="password"></Label>
-
               <Col xs="12" md={{ size: 6, offset: 3 }}>
-              <Input type="password" placeholder="password" id="password" required onChange={e => setPassword(e.target.value)} />
+              <Input type="password" placeholder="password" id="password" required />
               </Col>
-              
             </FormGroup>
+
             <FormGroup className="text-center">
               <Label check>
                 <Input type="checkbox" /> Remember me
@@ -72,15 +45,16 @@ fetch('/db.json')
             </FormGroup>
 
             <div className="d-flex justify-content-center">
-  <Button color="primary" className="btn-block" onClick={handleLogin}>Sign In</Button>
-</div>
+              <Button color="primary" className="btn-block" onClick={handleLogin}>Sign In</Button>
+            </div>
 
             <br></br>
-            
+
             <div className="text-center">
-              <a href="#1">Forgot password?</a>
-              <p>Not a member yet? <a href="#1" onClick={handleClick}>Sign Up here</a></p>
+              {/* <a href="#1">Forgot password?</a> */}
+              <p>Not a member yet? <a href="#Sign Up" onClick={handleClick}>Sign Up here</a></p>
             </div>
+
           </Form>
         </Col>
       </Row>
@@ -90,42 +64,39 @@ fetch('/db.json')
           <Col xs="12" md={{ size: 6, offset: 3 }}>
             <Form>
               <h3 className="text-center">Sign Up</h3>
+
               <FormGroup>
                 <Label for="username"></Label>
-
-                <Col xs="12" md={{ size: 6 }}>
-                <Input type="text" placeholder="User Name" id="username" required className='form-control' />
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="text" placeholder="User Name" id="username" required className='form-control' />
                 </Col>
-               
               </FormGroup>
+
               <FormGroup>
                 <Label for="email"></Label>
-
-                <Col xs="12" md={{ size: 6 }}>
-                <Input type="email" placeholder="Email" id="email" required className='form-control'/>
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="email" placeholder="Email" id="email" required className='form-control'/>
                 </Col>
-                
               </FormGroup>
+
               <FormGroup>
                 <Label for="password"></Label>
-
-                <Col xs="12" md={{ size: 6 }}>
-                <Input type="password" placeholder="Password" id="password" required className='form-control' />
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="password" placeholder="Password" id="password" required className='form-control' />
                 </Col>
-                
               </FormGroup>
 
-              
-              <Button color="success" className="btn-block">Sign Up</Button>
-              
+              <Col md={{ size: 6, offset: 5 }}>
+                <Button color="primary" className="btn-block" onClick={handleSignup}>Sign Up</Button>
+              </Col>
             </Form>
           </Col>
         </Row>
-      ) : null }
+      )
+   : null }
 
     </Container>
   );
 }
 
 export default Login;
-
