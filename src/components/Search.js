7b96cdd2b2f946c51076727d170c6eb38baa3efb
git_Vickyprinz <kariukivictor315@gmail.com>
2023-01-26 12:54:13 +0300

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,56 @@
+import React, { useState, useEffect } from 'react';
 
-const Search = ({addToShelf,results,isLoading,handleSearchInput,searchInput}) => {
+const Search = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [results, setResults] = useState([]);
+  const [allResults, setAllResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=books+&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`
+      );
+      const data = await response.json();
+      setAllResults(data.items);
+      setResults(data.items);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const handleSearchInput = async event => {
+    setSearchInput(event.target.value);
+    setIsLoading(true);
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${event.target.value}&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`);
+    const data = await response.json();
+    setAllResults(data.items);
+    setResults(data.items);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    const filteredResults = allResults
+      .filter(result =>
+        result.volumeInfo.title.toLowerCase().includes(searchInput.toLowerCase())
+      )
+      .sort((a, b) => {
+        const titleA = a.volumeInfo.title.toLowerCase();
+        const titleB = b.volumeInfo.title.toLowerCase();
+        const search = searchInput.toLowerCase();
+        if (titleA.startsWith(search) && !titleB.startsWith(search)) {
+          return -1;
+        }
+        if (!titleA.startsWith(search) && titleB.startsWith(search)) {
+          return 1;
+        }
+        return 0;
+      });
+      
+    setResults(filteredResults);
+  }, [searchInput, allResults]);
 
   return (
     <div>
@@ -14,28 +65,31 @@ const Search = ({addToShelf,results,isLoading,handleSearchInput,searchInput}) =>
             onChange={handleSearchInput}
           />
         </div>
+        <button
+          type="button"
+          className="btn btn-outline-warning m-2"
+          onClick={() => setResults(allResults)}
+        >
+          Search
+        </button>
       </form>
       <div className='container'>
         {isLoading ? (
           <div>Loading...</div>
         ) : (
-          <div className=' d-flex  row  mb-3 '>
+          <div className=' d-flex  row mb-3 '>
             {results.map((result) => (
-              <div key={result.id}  className= "card mb-5 me-4 bg-warning" style={{width: "21rem"}} >
+              <div key={result.id}  className= "card mb-5 me-3 bg-warning" style={{width: "21rem"}} >
                   {result.volumeInfo.imageLinks && (
                   <img src={result.volumeInfo.imageLinks.smallThumbnail  } alt={result.volumeInfo.title} className="card-img-top  img-fluid"  />
                 )}
               <div className='card-body '>
-                <div className="d-flex justify-content-between">
-                   <h5 className='card-title'>{result.volumeInfo.title}</h5>
-                   <p className="badge  bg-dark text-light"> pages: {result.volumeInfo.pageCount}</p>
-                </div>
-                
-                   <p className='card-text'> { result.volumeInfo.authors}</p>
-                   <p className='card-text'> {result.searchInfo.textSnippet}</p>
+                <h5 className='card-title'>{result.volumeInfo.title}</h5>
+                <p className='card-text'> Author: :{ result.volumeInfo.authors}</p>
+<p className='card-text'> {result.volumeInfo.description}</p>
                 <div className='d-flex justify-content-between'>
-                 <a className='card-text btn btn-outline-dark' target='_blank' href={result.volumeInfo.previewLink}>buy book</a>
-                <button className='btn btn-outline-dark' onClick={() => addToShelf(result)}>add to shelf</button>
+                <a className='card-text btn btn-outline-dark' target='_blank' href={result.volumeInfo.previewLink}>buy book</a>
+                <button className='btn btn-outline-dark'>add to shelf</button>
             </div>
           </div>
           </div>
@@ -48,4 +102,3 @@ const Search = ({addToShelf,results,isLoading,handleSearchInput,searchInput}) =>
 };
 
 export default Search;
-
