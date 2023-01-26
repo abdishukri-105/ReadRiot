6a21588856a922fedecc1cc3e2a9b9525607e2b5
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-26 22:35:09 +0300

    updated card

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,34 +7,34 @@ return (
       <div className="jumbotron card">
        <img src="./images/bg.jpg " className="card-img img-fluid" alt="bookshelf" />
       <div className="card-img-overlay ">
-       <p className="text-center mt-4 text-warning" style={{ fontFamily: 'Open Sans', fontSize: '3rem',fontWeight: 'bold'}}>
+       {/* <p className="text-center mt-4 text-dark" style={{ fontFamily: 'Open Sans', fontSize: '3rem',fontWeight: 'bold'}}>
          READRIOT -  your no. 1  book discovery platform
-       </p>
-       <div className="d-flex justify-content-around">
-        <div className="mt-4 p-3 card  col-5">
-         <p className="  fs-5 fw-bold" style={{lineHeight: "3rem", fontSize: "1rem" }}>
+       </p> */}
+       <div className="d-flex justify-content-around mt-5">
+        <div className="mt-4 p-5 card  col-5">
+         <p className="  fs-5 fw-bold" style={{lineHeight: "3rem" }}>
            Our app is built on the Google Books API, which means you have access to millions of books, including best sellers, classic literature, and new releases.
          </p>
       
         </div>
      <div className="card col-4 mt-4 justify-content-center">
-       <h4 className="ms-5 m-3 mt-4 fw-bold">please sign In to continue</h4>
+       <h4 className="ms-5 m-3 mt-4 fw-bolder">please sign In to continue</h4>
         <form className="col-8  ms-5">
          <div class="form-group mb-3">
-          <label for="username">Username</label>
+          <label for="username " className="mb-2 fw-bold">Username</label>
           <input type="text" class="form-control" id="username" placeholder="Enter username" required/>
         </div>
         <div class="form-group mb-3">
-          <label for="password">Password</label>
+          <label for="password" className="mb-2 fw-bold">Password</label>
           <input type="password" class="form-control" id="password" placeholder="Enter password" required/>
         </div>
         <Link to ="/search">
            <button className="btn btn-outline-warning mb-4"> SIGN IN</button> 
          </Link>
          
-         <p>Dont have account? 
+         <p className="fw-bold">Dont have account? 
             <Link to="/register">
-              <button className="btn btn-outline-warning">sign up</button> 
+              <button className="btn btn-outline-warning ms-3">sign up</button> 
             </Link>
           </p>
           
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-// import '../App.css';
+import '../App.css';
 
 const Search = ({addToShelf,results,isLoading,handleSearchInput,searchInput}) => {
 
@@ -18,26 +18,26 @@ const Search = ({addToShelf,results,isLoading,handleSearchInput,searchInput}) =>
         </div>
         
       </form>
-      <div className='container-search'>
+      <div className='container justify-content-center'>
         {isLoading ? (
           <div>Loading...</div>
         ) : (
-          <div className=' d-flex  row  mb-3 card-section '>
+          <div className=' card-section ms-4 row   card-section '>
             {results.map((result) => (
-              <div key={result.id}  className= "card mb-5 me-4 bg-warning" style={{width: "21rem"}} >
+              <div key={result.id}  className= "card book-holder mb-5 me-4 bg-warning"  >
                   {result.volumeInfo.imageLinks && (
-                  <img src={result.volumeInfo.imageLinks.smallThumbnail  } alt={result.volumeInfo.title} className="card-img-top  img-fluid"  />
+                  <img src={result.volumeInfo.imageLinks.smallThumbnail  } alt={result.volumeInfo.title} className="card-img-top card-image img-fluid"  />
                 )}
               <div className='card-body '>
                 <div className="d-flex justify-content-between">
-                   <h5 className='card-title'>{result.volumeInfo.title}</h5>
-                   <p className="badge  bg-dark text-light"> pages: {result.volumeInfo.pageCount}</p>
+                   <h6 className='card-title fw-bold'>{result.volumeInfo.title}</h6>
+                   {/* <p className="badge  bg-dark text-light"> pages: {result.volumeInfo.pageCount}</p> */}
                 </div>
                    <p className='card-text'> { result.volumeInfo.authors}</p>
-                   <p className='card-text'> {result.searchInfo.textSnippet}</p>
-                <div className='d-flex justify-content-between'>
-                 <a className='card-text btn btn-outline-dark' target='_blank' href={result.volumeInfo.previewLink}>buy book</a>
-                <button className='btn btn-outline-dark' onClick={() => addToShelf(result)}>add to shelf</button>
+                   {/* <p className='card-text'> {result.searchInfo.textSnippet}</p> */}
+                <div className=' d-flex justify-content-between'>
+                 <a className=' btn btn-outline-dark' target='_blank' href={result.volumeInfo.previewLink}>see more</a>
+                <button className=' btn btn-outline-dark' onClick={() => addToShelf(result)}>add to shelf</button>
             </div>
           </div>
           </div>
