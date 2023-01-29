91b85e1da7d77e585b8d9ca0c70561a51ca25c6c
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-29 22:02:33 +0300

    final touch

--- src/components/App.js
+++ src/components/App.js
@@ -48,54 +48,7 @@ function App() {
     console.log(newresult)
   }
  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  //     const response = await fetch(
-  //       `https://www.googleapis.com/books/v1/volumes?q=books+&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`
-  //     );
-  //     const data = await response.json();
-  //     console.log(data);
-  //     setAllResults(data.items);
-  //     setResults(data.items);
-  //     setIsLoading(false);
-  //   };
-  //   fetchData();
-  // }, []);
-   
-
-  // const handleSearchInput = async event => {
-  //   setSearchInput(event.target.value);
-  //   setIsLoading(true);
-  //   const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${event.target.value}&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`);
-  //   const data = await response.json();
-  //   setAllResults(data.items);
-  //   setResults(data.items);
-  //   setIsLoading(false);
-  // }
-
-
-    
-  // useEffect(() => {
-  //  const filteredResults = allResults
-  //   .filter(result =>
-  //     result.volumeInfo.title.toLowerCase().includes(searchInput.toLowerCase())
-  //   )
-  //   .sort((a, b) => {
-  //     const titleA = a.volumeInfo.title.toLowerCase();
-  //     const titleB = b.volumeInfo.title.toLowerCase();
-  //     const search = searchInput.toLowerCase();
-  //     if (titleA.startsWith(search) && !titleB.startsWith(search)) {
-  //       return -1;
-  //     }
-  //     if (!titleA.startsWith(search) && titleB.startsWith(search)) {
-  //       return 1;
-  //     }
-  //     return 0;
-  //   });
-  //   setResults(filteredResults);
-  // }, [searchInput, allResults]);
-
+ 
 
   
   const navigate = useNavigate()
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -5,18 +5,14 @@ const Shelf = ({shelf, removeFromShelf}) => {
     return ( 
          
     <div className=" pt-5 ml-4 mr-4 grid grid-cols-5 gap-2   ">
-          {/* <h2 className="fw-bold text-center mt-4"> Your Shelf</h2> */}
+        
           {shelf.length > 0 ? ( 
             shelf.map((book) => (
               <div key={book.id} className="p-2 relative w-56 max-w-sm rounded overflow-hidden shadow-lg bg-amber-400" >
                   <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} className=" w-56 rounded-xl h-64  transform  hover:scale-105 transition duration-150 ease-in-out" />
                   <div className="p-1">
-                     
-                        <p className='font-bold'>{book.volumeInfo.title}</p>
-                     
-                     
+                      <p className='font-bold'>{book.volumeInfo.title}</p>
                       <p className=' p-1 italic '> { book.volumeInfo.authors}</p>
-                   
                     <button className="inline-block px-3 py-1 bg-yellow-700 text-white  font-medium text-xs leading-tight uppercase rounded hover:bg-yellow-600  hover:text-black" onClick={() => removeFromShelf(book.id)}>remove from shelf</button>
                   </div>
               </div>
