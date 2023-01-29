7758027c4deff3e17b77acb5eb66236070a3eaa4
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-29 21:22:35 +0300

    shelf card ui fix

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,21 +1,23 @@
+import '../App.css';
+
 const Shelf = ({shelf, removeFromShelf}) => {
 
     return ( 
          
-    <div className="container d-flex column mt-4">
+    <div className=" pt-5 ml-4 mr-4 grid grid-cols-5 gap-2   ">
           {/* <h2 className="fw-bold text-center mt-4"> Your Shelf</h2> */}
           {shelf.length > 0 ? ( 
             shelf.map((book) => (
-              <div key={book.id} className="card bg-warning  me-4" style={{width: "20rem" }}>
-                  <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} className="card-img-top img-fluid" style={{height: "15rem", width: "20rem"}}/>
-                  <div className="card-body">
-                      <div className="d-flex justify-content-between">
-                        <p className='card-title'>{book.volumeInfo.title}</p>
-                        <p className="badge  bg-dark text-light"> pages: {book.volumeInfo.pageCount}</p>
-                      </div>
-                      <p className='card-text'> { book.volumeInfo.authors}</p>
-                      <p className='card-text'> {book.searchInfo.textSnippet}</p>
-                    <button className="btn btn-dark border-0" onClick={() => removeFromShelf(book.id)}>remove from shelf</button>
+              <div key={book.id} className="p-2 relative w-56 max-w-sm rounded overflow-hidden shadow-lg bg-amber-400" >
+                  <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} className=" w-56 rounded-xl h-64  transform  hover:scale-105 transition duration-150 ease-in-out" />
+                  <div className="p-1">
+                     
+                        <p className='font-bold'>{book.volumeInfo.title}</p>
+                     
+                     
+                      <p className=' p-1 italic '> { book.volumeInfo.authors}</p>
+                   
+                    <button className="inline-block px-3 py-1 bg-yellow-700 text-white  font-medium text-xs leading-tight uppercase rounded hover:bg-yellow-600  hover:text-black" onClick={() => removeFromShelf(book.id)}>remove from shelf</button>
                   </div>
               </div>
               ))
